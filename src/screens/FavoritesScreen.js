11db42d2924a5a4f00309afe4dc7bb2320344a04
DascalowPC <rauldascalu34@gmail.com>
2026-06-03 20:00:45 +0300

    Am adaugat folderul de screens

---
+++ src/screens/FavoritesScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Aici va fi lista cu caractere favorite</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#f5f5f5' },
+  text: { fontSize: 18, fontWeight: 'bold' }
+});
